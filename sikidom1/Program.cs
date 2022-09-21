4c3b0794b19948ed5c1eb2d6fc84bef3c7d2fc52
TDV <TDV@DESKTOP-338B4M5>
2022-09-21 21:44:48 +0200

    Add project files.

---
+++ sikidom1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sikidom1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Kocka kocka = new Kocka(9);
+                Console.WriteLine($"Kocka oldala: {kocka.OldalA} \n\t Területe:{kocka.Terulet()}\n\t Kerülete:{kocka.Kerulet()}");
+            }
+            catch (ArgumentOutOfRangeException ad)
+            {
+                Console.WriteLine(ad.Message);
+            }
+
+            Console.WriteLine("Program vége");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ sikidom1/kocka.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sikidom1
+{
+    internal class Kocka
+    {
+        double oldalA;
+
+        public double OldalA
+        {
+            get => oldalA;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException($"{value} érték nem megengedett a kocka oldalának");
+                }
+                else
+                {
+                    oldalA = value;
+                }
+            }
+        }
+
+        public Kocka(double oldalA)
+        {
+            OldalA = oldalA;
+        }
+        public double Terulet()
+        {
+            return oldalA * oldalA;
+        }
+        public double Kerulet()
+        {
+            return 4 * oldalA;
+        }
+    }
+}
